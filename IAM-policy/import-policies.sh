64df8f8ed07b2454bf39a8cb45ffdfe12619dde6
aka <akahane.yasuhiko@gmail.com>
2021-06-21 22:55:14 +0900

    mv

--- IAM-policy/import-policies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# import policies from {PlicyName}.json
-for filename in *.json; do
-    echo $filename
-    echo ${filename%.*}
-    echo "aws iam create-policy --policy-name ${filename%.*} --policy-document file://$filename"
-    aws iam create-policy --policy-name ${filename%.*} --policy-document file://$filename
-done
